257382096c3818c2927f6d661f2d0798694c2002
DimitarGrozev5 <dimgrozev5@gmail.com>
2022-07-03 19:29:26 +0300

    phone sanitizition function

---
+++ src/utils/data-sanitizition.js
@@ -0,0 +1,6 @@
+export const sanitizePhone = (phone) => {
+  const noWhitespace = phone.replaceAll(" ", "");
+  const noCountryCode = noWhitespace.replace("+359", "0");
+
+  return noCountryCode;
+};
