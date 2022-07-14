62b8036d31a4e72d058205a4610d42f5b7f0d873
DimitarGrozev5 <dimgrozev5@gmail.com>
2022-07-14 22:34:50 +0300

    Created HTTP Client hook

---
+++ src/hooks/useHttpClient.js
@@ -0,0 +1,90 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+import { useSelector } from "react-redux";
+import { baseUrl } from "../constants/baseUrl";
+
+export const useHttpClient = () => {
+  const token = useSelector((state) => state.user.isLoggedIn);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+
+  const activeHttpRequests = useRef([]);
+
+  const sendRequest = useCallback(
+    async (url, body = null, { method, headers, auth, notJSON }) => {
+      setIsLoading(true);
+
+      const httpAbortCtrl = new AbortController();
+      activeHttpRequests.current.push(httpAbortCtrl);
+
+      // Setup config object for fetch request
+      const config = {};
+      const headersObj = {};
+
+      // Set http method
+      config.method = "GET";
+      if (method) {
+        config.method = method;
+      }
+      if (!method && body && !notJSON) {
+        config.method = "POST";
+      }
+
+      // Convert body to json and attach it
+      if (body) {
+        config.body = notJSON ? body : JSON.stringify(body);
+      }
+
+      // Set Content-Type to json
+      if (!headers?.["Content-Type"] && body && !notJSON) {
+        headersObj["Content-Type"] = "application/json";
+      }
+
+      // Add Authorization token
+      if (auth) {
+        headersObj["Authorization"] = "Bearer " + token;
+      }
+
+      // Merge provided headers and preconfigured headers
+      config.headers = { ...headersObj, ...headers };
+
+      // Attach httpAbort controller
+      config.signal = httpAbortCtrl.signal;
+
+      try {
+        // Fetch data
+        const response = await fetch(baseUrl + url, config);
+
+        // Convert to json
+        const responseData = await response.json();
+
+        // Remove http abort controller from list of active controllers
+        activeHttpRequests.current = activeHttpRequests.current.filter(
+          (reqCtrl) => reqCtrl !== httpAbortCtrl
+        );
+
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+
+        setIsLoading(false);
+        return responseData;
+      } catch (err) {
+        setError(err.message);
+        setIsLoading(false);
+        throw err;
+      }
+    },
+    [token]
+  );
+
+  useEffect(
+    () => () => {
+      activeHttpRequests.current.forEach((abortCtrl) => abortCtrl.abort());
+    },
+    []
+  );
+
+  const clearError = () => setError(null);
+
+  return { isLoading, error, sendRequest, clearError, setError };
+};
