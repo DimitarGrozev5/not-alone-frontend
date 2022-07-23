8d8758b6b5efaadd49c1f2faa61110bc9d87aed0
DimitarGrozev5 <dimgrozev5@gmail.com>
2022-07-23 22:10:10 +0300

    Added Portal to LoadingSpinner

--- src/components/UIComponents/LoadingSpinner/LoadingSpinner.js
+++ src/components/UIComponents/LoadingSpinner/LoadingSpinner.js
@@ -1,12 +1,14 @@
+import ReactDOM from "react-dom";
 import React from "react";
 
 import styles from "./LoadingSpinner.module.css";
 
 const LoadingSpinner = (props) => {
-  return (
+  return ReactDOM.createPortal(
     <div className={props.asOverlay && styles["loading-spinner__overlay"]}>
       <div className={styles["lds-dual-ring"]}></div>
-    </div>
+    </div>,
+    document.getElementById("modal-root")
   );
 };
 
