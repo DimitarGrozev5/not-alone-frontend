ce7810407aebe59541c0c01a6b35a75b4b877fe9
DimitarGrozev5 <dimgrozev5@gmail.com>
2022-08-04 22:49:02 +0300

    add getCache prop

--- src/hooks/useLoadPageData.js
+++ src/hooks/useLoadPageData.js
@@ -1,9 +1,14 @@
 import { useCallback, useEffect, useState } from "react";
 import { useHttpClient } from "./useHttpClient";
 
-export const useLoadPageData = (loadUrl, { auth = true } = {}) => {
+export const useLoadPageData = (
+  loadUrl,
+  { auth = true, getCache = false } = {}
+) => {
   const { isLoading, error, sendRequest, clearError, setError } =
     useHttpClient();
+
+  // const [source, setSource] = useState("");
 
   const [data, setData] = useState(null);
   useEffect(() => {
